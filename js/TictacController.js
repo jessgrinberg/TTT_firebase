7c622068d2edf7ecf5ecbbe8c132d53fc05bc1c7
Your Name <jessgrinberg@gmail.com>
2015-05-16 16:06:19 -0700

    player turn displaying / working with FB

--- js/TictacController.js
+++ js/TictacController.js
@@ -23,6 +23,8 @@ function TictacController($firebaseObject, $index){ //constructor functin that b
     self.appearGame = appearGame;
     self.appear = false;
 
+    self.turn = "";
+
 
     function getBoard(){
 // counter++;
@@ -71,12 +73,14 @@ function TictacController($firebaseObject, $index){ //constructor functin that b
             self.board.boxes[$index].active[$index]= true;
             self.board.counter ++;
             console.log(self.board.counter);
+            self.board.turn = "player 2 turn";
             self.board.$save(); 
             self.playerX = false ;   
             } else {
             self.board.boxes[$index].p1owns = "O";
             console.log("O");
             self.board.boxes[$index].active[$index] = false;
+            self.board.turn = "player 1 turn";
             self.board.counter ++;
             console.log(self.board.counter);
             self.board.$save(); 
@@ -103,7 +107,7 @@ console.log(self.win)
         ((self.board.boxes[2].p1owns == "X") && (self.board.boxes[4].p1owns == "X") && (self.board.boxes[6].p1owns == "X")) 
       ) { 
           console.log("player one wins");
-          self.board.message = " Player One Wins !";
+          self.board.message = self.newName + " Player One Wins !";
           self.board.$save(); 
  
           // self.board.$save();
@@ -122,7 +126,7 @@ console.log(self.win)
         ((self.board.boxes[2].p1owns == "O") && (self.board.boxes[4].p1owns == "O") && (self.board.boxes[6].p1owns == "O")) 
       ) {
           console.log("player two wins");
-          self.board.message =  " Player Two Wins ! ";
+          self.board.message = self.newName2 + " Player Two Wins ! ";
 
           self.board.$save();
           self.win = true;
@@ -143,6 +147,7 @@ console.log(self.win)
       self.playerX = true;
       self.playerO = false;
       self.win = false;
+      self.board.turn = "";
       self.board.message = "";
       self.board.boxes = [
         {active: false, p1owns:""},
@@ -159,12 +164,57 @@ console.log(self.win)
       // self.counter = 0;
       // console.log(self.clearGrid);
      //self.buttons[0],1
-  }
+  }     
 
-      function appearGame(){
-      self.appear = !self.appear;
+// self.addTodo = addTodo;
+// <form class="add-todo" ng-submit="todosCtrl.addTodo()">
+// <input class="text-box" type="text" placeholder="I need to..." ng-model="todosCtrl.text">
+// <input type="submit" class="btn btn-add" value="+">
+// </form>
 
-      }
+// function addTodo(){
+//   self.todos.$add({task: self.text, done: false});
+//    self.text = null;
+//  }
+
+
+
+    self.names = [];
+    self.newName = " ";
+    self.addNames = addNames;
+
+
+
+      // function addTodo(){
+      //       self.todos.$add({task: self.text, done: false});
+      //       self.text = null;
+      //   }
+
+// self.addNames = addNames;
+
+    // function addNames()  {
+    //     self.names.$add();
+    // }
+    
+    function addNames(){
+    self.names.push(self.newName);
+    self.board.$save();
+
+    // self.board.names.$add({player: self.newName});
+    }
+
+
+    // self.names2 = [];
+    // self.newName2 = " ";
+    // self.addNames2 = addNames2;
+
+    // function addNames2(){
+    // self.names2.push(self.newName2);
+    // }
+
+    function appearGame(){
+    self.appear = !self.appear;
+    }
 
 
 }
